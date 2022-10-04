ce50094ce502deb2d7483e173dadad97e445453b
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2022-10-04 02:20:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.sessionName = "session"
 
 // Prefix //
 // 🎗don't change the prefix if you can change the prefix may be bot doesn't work properly//
-global.prefa = ['-'] 
+global.prefa = ['  , ! , . , 🐦 , # , $ '] 
 
 
 global.location = "Egypt,Egypt"
